eea4fc46aed5c4236b7477dcea39e857fbe8e964
Abishek Shukla <dav.it.abhishek@gmail.com>
2024-05-06 18:37:01 +0530

    123

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String args[]){
+        RWOnly object=new RWOnly();
+
+        //System.out.println(object.string);
+        //java: string has private access in com.driver.RWOnly
+
+        object.setName("Encapsulation in Java");
+        String string=object.getName();
+        System.out.println(string);
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,17 @@
 package com.driver;
 
 public class RWOnly {
-    
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String string) {
+        this.name = string;
+    }
+
+
+
 }
